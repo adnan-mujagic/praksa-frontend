3b98f902795ec17a54d355d485419b42cff496f8
adnan-mujagic <adnanmujagic@outlook.com>
2021-06-26 19:47:39 +0200

    Loading all posts into dashboard from the api

--- front/src/components/Dashboard/Dashboard.js
+++ front/src/components/Dashboard/Dashboard.js
@@ -3,13 +3,33 @@ import "./Dashboard.css";
 import Post from "../Post/Post";
 
 class Dashboard extends React.Component{
-    
+    state = {
+        loading:true,
+        posts: []
+    }
+
+    async componentDidMount() {
+        const url="https://mediasell.herokuapp.com/api/posts";
+        const response = await fetch(url);
+        const results = await response.json();
+        this.setState({posts: results.data, loading:false})
+        console.log(results.data);
+    }
+  
+    componentWillUnmount() {
+    }
 
     render(){
         return(
             <div className="dashboard">
-                <Post name="iPhone" cost="500" state="New" description="new iPhone it is amazing!" product_picture="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-red-select-2020?wid=940&hei=1112&fmt=png-alpha&.v=1604343703000"/>
-                <Post name="Samsung Galaxy" cost="299" state="Used" description="Very cool economic phone. Reliable" product_picture="https://images.samsung.com/is/image/samsung/assets/hr/galaxy-a52/pcd/a-category/img_bnn_galaxy_device.png?$ORIGIN_PNG$" />
+                {this.state.loading || !this.state.posts?
+                <div>Loading...</div>:
+                
+                <div>{this.state.posts.map(post => (    
+                    <Post name={post.name} cost={post.cost} description={post.description} state={post.state} product_picture={post.image_url} />
+                ))}
+                
+                </div>}
             </div>
         )
     }
