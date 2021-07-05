48e1b989e483e1ec24f36c00a38b65196580f005
adnan-mujagic <adnanmujagic@outlook.com>
2021-07-05 16:01:18 +0200

    Profile Card Image clickable

--- front/src/components/ProfileCard/ProfileCard.js
+++ front/src/components/ProfileCard/ProfileCard.js
@@ -17,12 +17,12 @@ export default class ProfileCard extends React.Component{
             
             <div className="profile-card">
                 <div className="profile-card-top">
-                    <div className="profile-card-img">
+                    <div className="profile-card-img" onClick={this.onVisitProfile}>
                         <img src={this.props.profile_picutre} alt="User"/>
                     </div>
                     <div className="profile-card-credentials">
                         <div className="profile-card-full-name">{this.props.full_name}</div>
-                        <div className="profile-card-username">@{this.props.username}</div>
+                        <div className="profile-card-username" onClick={this.onVisitProfile}>@{this.props.username}</div>
                     </div>
                 </div>
                 <div className="profile-card-other">
